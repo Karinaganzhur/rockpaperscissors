2bc1bf125839735b8ce35906f3653a0dcd9a34d0
Karina Ganzhur <karina02.10.99@gmail.com>
2018-12-03 21:17:48 +0000

    final

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
@@ -8,29 +9,108 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
-var userChoice;
-userChoice ="";
-$("#shoot").click(function() {
-    var userChoice = $("#input").val();
- $("#userChoice").text(userChoice);
+
+
+$("#shoot").click(function(){
+    var userChoice = $("#input").val().toLowerCase();
+    rockPaperScissor(userChoice);
+    $("#userChoice").text(userChoice);
+});
+ var userChoice;
+ userChoice="rock"; 
+ userChoice="paper"; 
+ userChoice="scissors"; 
+
+function rockPaperScissor(userChoice){
+    let myArray = [];
+    let computerChoice = Math.floor(Math.random() * 3);
+    
+    //display the result
+    if(userChoice === "rock"){
+        myArray = ["scissors", "rock", "paper"];
+    } else if(userChoice === "scissors"){
+        myArray = ["paper", "scissors", "rock"];
+    } else if(userChoice === "paper"){
+        myArray = ["rock", "paper", "scissors"];
+    }
+    
+    $("#computerChoice").html(myArray[computerChoice]);
+    
+    
+    logic(computerChoice);
+}
+
+function logic(computerChoice){
+    let result;
+    if(computerChoice < 1){
+        result = "You Win!";
+    } else if(computerChoice === 1){
+        result = "It is a Tie!";
+    } else if(computerChoice > 1){
+        result = "You Lose!";
+    }
+    $("#result").html(result);
+}
+
+ var computerChoice = Math.random();
+
+ var winner;
+ winner="";
+
+ $("#shoot").click(function(){
+ var userChoice = $("#userChoice").val();
+ var computerChoice = $("#computerChoice").val();
+ var winner = $("#result").val();
+}); 
+
+ $("#shoot").click(function(){
+  if(computerChoice > .888 && userChoice==="rock") {
+         $("#result").html("There is a tie"); 
+     }
+     else  if(computerChoice > .888 && userChoice==="paper") {
+         $("#result").html("User wins!");  
+    }
+     else  if(computerChoice > .888 && userChoice==="scissors") {
+         $("#result").html("Computer wins"); 
+    }
+    else  if(computerChoice > .555 && userChoice==="scissors") {
+         $("#result").html("User wins!"); 
+    }
+     else  if(computerChoice > .555 && userChoice==="paper") {
+       $("#result").html("There is a tie"); 
+     }
+     else  if(computerChoice > .555 && userChoice==="rock") {
+        $("#result").html("Computer wins!"); 
+} 
+     else  if(computerChoice < .555 && userChoice==="rock") {
+        $("#result").html("User wins!"); 
+     } 
+      else  if(computerChoice < .555 && userChoice==="scissors") {
+        $("#result").html("There is a tie"); 
+     } 
+    else  if(computerChoice < .555 && userChoice==="scissors") {
+       $("#result").html("There is a tie"); 
+    } 
+     else{
+         $("#result").html("Try again!"); 
+    }
 });
 
 
+ var randomNumber;
+ randomNumber= 0;
 
-
-var randomNumber = 0;
-     $("#shoot").click(function(){
-var randomNumber = Math.random();
- console.log(randomNumber);
-    if( randomNumber> .801) {
+ $("#shoot").click(function(){
+ var computerChoice = Math.random();
+     console.log(computerChoice);
+   
+     if(computerChoice > .888) {
+         $("#computerChoice").html("rock"); 
+  }
+    else if(computerChoice > .555){
         $("#computerChoice").html("paper"); 
     }
-    else if(randomNumber> .609){
-        $("#computerChoice").html("rock");}
-     else {
-        $("#computerChoice").html("scissors");
-         
-     }
-     });
-     
-var winner;
+    else {
+       $("#computerChoice").html("scissors");   
+   }
+});
